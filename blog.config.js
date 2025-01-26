c31a2b9cad9e654bc7dbf04133ee70b2f7119ef2
yuelin.ye <yuelin.ye@xbongbong.com>
2025-01-26 15:14:19 +0800

    🌐 Set blog language to Chinese (Simplified)

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   email: 'yuelinyes@gmail.com',
   link: '',
   description: '记录',
-  lang: 'en-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
